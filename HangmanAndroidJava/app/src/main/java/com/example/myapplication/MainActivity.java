4ff94373e526b5a5fb5607fb611595335f951918
Daniel Blanton <dblantonnc@gmial.com>
2024-03-18 21:28:07 -0400

    addin 3 comments

--- HangmanAndroidJava/app/src/main/java/com/example/myapplication/MainActivity.java
+++ HangmanAndroidJava/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,28 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        //finds button
+        Button play = findViewById(R.id.button);
+        //sets button to on click
+        play.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // starts new activity/ aka new page 
+                startActivity(new Intent(MainActivity.this, hang_game.class));
+            }
         });
+
     }
 }
