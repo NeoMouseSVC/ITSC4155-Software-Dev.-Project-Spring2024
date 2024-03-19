ce7b30e37fc8ba3a4bbb68e69234a71d05b34a79
Jacob Taylor <jacobjtaylor0@gmail.com>
2024-03-19 19:22:05 -0400

    Adding Basic parts to Customize Section

--- HangmanAndroidJava/app/src/main/java/com/example/myapplication/customize_screen.java
+++ HangmanAndroidJava/app/src/main/java/com/example/myapplication/customize_screen.java
@@ -1,24 +1,64 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
 
 public class customize_screen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customize_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+            Button btnCharacter = findViewById(R.id.btnCharacter);
+            btnCharacter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainerView, CharacterFragment.class, null)
+                            .setReorderingAllowed(true)
+                            .addToBackStack("name") // Name can be null
+                            .commit();
+                }
+            });
+
+        Button btnTheme = findViewById(R.id.btnTheme);
+        btnTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, ThemeFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
         });
+
+        Button btnStore = findViewById(R.id.btnStore);
+        btnStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, StoreFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+
+
+        };
     }
-}
