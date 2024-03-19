173fb32b3830531ec385e13597f09ebe433214ff
Daniel Blanton <dblantonnc@gmial.com>
2024-03-19 15:34:27 -0400

    addin other pages to work on

--- HangmanAndroidJava/app/src/main/java/com/example/myapplication/MainActivity.java
+++ HangmanAndroidJava/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,12 +16,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //finds button
         Button play = findViewById(R.id.button);
+        Button diff = findViewById(R.id.button3);
+        Button custom = findViewById(R.id.button2);
+        ImageView set = findViewById(R.id.imageView2);
         //sets button to on click
         play.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                // starts new activity/ aka new page 
+                // starts new activity/ play button
                 startActivity(new Intent(MainActivity.this, hang_game.class));
+            }
+        });
+        diff.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // starts new activity/ play button
+                startActivity(new Intent(MainActivity.this, diffculty_screen.class));
+            }
+        });
+        custom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // starts new activity/ play button
+                startActivity(new Intent(MainActivity.this, customize_screen.class));
+            }
+        });
+        set.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // starts new activity/ play button
+                startActivity(new Intent(MainActivity.this, settings_screen.class));
             }
         });
 
